08b988c12af3234331e635e79814edfa1a2a2b5b
Apple <fibo2944@yahoo.co.jp>
2019-01-30 23:21:17 +0900

    スタートボタンからスタートして0.01秒ごとに画像の大きさを変えることで滑らかに見えるようにした。 あとストップ、リセット機能がまだ。 カウントがなくなってから画像が変わるまでの時間がとても長い不具合がある。

--- timeLimit/ViewController.swift
+++ timeLimit/ViewController.swift
@@ -9,17 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    var count = 500
+    let maxCount = 500
+    
+    let img = UIImage(named: "Unknown")
+    
+    let img2 = UIImage(named: "bang")
+    
+    var timer = Timer()
+    
+    var flag = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    
+    @IBAction func startButtonTapped(_ sender: Any) {
+        if timer == nil{
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: Selector("countDown"), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+    }
+    
+    @objc func countDown(){
+        count-=1
+        let min = count/60/100
+        let sec = (count/100)%60
+        timeLabel.text = "\(min):\(sec)"
+        myImageView.image = img
+    
+        let rate = Double(maxCount-count) / Double(maxCount)
+        
+        myImageView.transform = CGAffineTransform(scaleX: CGFloat(rate), y: CGFloat(rate))
+        
+        if count == 0{
+            timer.invalidate()
+            self.myImageView.image = self.img2
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
